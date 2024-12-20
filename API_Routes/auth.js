ca58c114b05059b22ec2e8c07f3b6b8033db1632
Asiya <asiyacode@gmail.com>
2024-12-20 16:47:44 +0530

    testing APIs

--- API_Routes/auth.js
+++ API_Routes/auth.js
@@ -1,6 +1,6 @@
 //API code
 import express from "express";
-import sendOTP from "../controllers/authcontrollers.js"
+import { sendOTP, verifyOTP } from "../controllers/authcontrollers.js"
 //importing actual functionality from controllers/authControllers.js
 
 const router = express.Router();
@@ -8,7 +8,7 @@ const router = express.Router();
 //Defining API routes
 
 router.post("/send-otp", sendOTP);
-// router.post("/verify-otp", verifyOTP);
+router.post("/verify-otp", verifyOTP);
 // router.post("/set-password", setPassword);
 // router.post("/login", login);
 
--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -5,11 +5,11 @@ import env from "dotenv";
 
 env.config();
 const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
@@ -28,17 +28,17 @@ const sendOTP = async(req, res) => {
 
   const password = req.body.password || "default_password";
   const query = "INSERT INTO users (phone_no, password) VALUES ($1, $2) RETURNING *";
- await db.query(query, [phone_no, password]);
+  await db.query(query, [phone_no, password]);
 
   //Generating OTP
   const otp = otpGenerator.generate(6, { digits:true, lowerCaseAlphabets:false, upperCaseAlphabets: false, specialChars: false });
   //hashing otp
   bcrypt.hash(otp, 10, async(err, hash) => {
     if(err){
-        console.log("error hashing otp");
+      console.log("error hashing otp");
     }else{
-        //saving otp in db
-        const saveOTP = await db.query("INSERT INTO users (OTP) VALUES ($1)", [otp]);
+      //saving otp in db
+      const saveOTP = await db.query("INSERT INTO users (OTP) VALUES ($1)", [otp]);
     }
   })
   //sending otp to phone_no
@@ -46,4 +46,60 @@ const sendOTP = async(req, res) => {
   res.status(200).json({ message: `opt sent successfully`})
 }
 
-export default sendOTP;
+const verifyOTP = async(req, res) => {
+  try{
+  //user ke pass se otp lena
+  const { phone_no, userOTP } = req.body;
+
+  //Debug otp and mobile no
+  console.log(`users No. ${phone_no}`);
+  console.log(`users OTP. ${userOTP}`);
+
+  const user = await db.query("SELECT * FROM users WHERE phone_no = $1", [phone_no]);
+
+  const userDbData = user.rows[0];
+
+  if(!userDbData){
+    return res.status(404).json({message: `user is not found`})
+  }
+  
+ //cleaning the stored opt
+
+ const storedOTP = userDbData.otp.trim();
+ console.log(`This is the stored OTP: ${hashedOTP}`);
+
+ const isMatch = await bcrypt.compare(userOTP, storedOTP)
+ if(isMatch){
+  console.log(`otp matched`);
+  res.status(200).json({ message: `otp verified`})
+ }else{
+  res.status(404).json({ message: `Invalid otp`})
+ }
+}catch{
+  console.log("error verifying otp")
+  res.status(404).json({ message: `something went wrong`})
+}
+  // bcrypt.hash(userOTP, 10, async(err, hash) => {
+  //   if(err){
+  //     console.log("error hashing user otp in verification", err)
+  //   }else{
+      //specific user ku nikalna
+
+      // const hashedOTP = userDbData.otp;
+      //debug
+      // console.log(`this is the stored OTP: ${hashedOTP}`);
+      //cleanign otp
+      // const cleanOTP = hashedOTP.trim();
+      //comparing otp provided by user with the OTP i have in store
+      // const result = await bcrypt.compare(userOTP, cleanOTP);
+
+      // console.log(result);
+  //   }
+  // })
+
+  // res.status(200).json({ message: `opt verified`})
+  
+};
+
+// export default sendOTP;
+export { sendOTP, verifyOTP };
